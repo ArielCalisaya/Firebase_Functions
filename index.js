db9321deb7517afc7bbb565ff327b7dcade4a8e1
Ariel Calisaya <calisayaariel62@gmail.com>
2020-01-27 19:51:26 -0300

    svAccount integrated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-
+const serviceAccount = require("../key/service_account.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
@@ -8,7 +8,44 @@ admin.initializeApp({
 
 
 
+const db = admin.firestore();
 
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
+});
+
+exports.getComments = functions.https.onRequest((req, res) => {
+    db.collection("comments")
+        .get()
+        .then(data => {
+            let comments = [];
+            data.forEach(doc => {
+                comments.push(doc.data());
+            });
+            return res.json(comments);
+        })
+        .catch(err => console.error(err));
+});
+
+exports.createComment = functions.https.onRequest((req, res) => {
+    if(req.method !== 'POST'){
+        return res.status(400).json({error: 'Method not allowed'})
+    }
+
+    const newComment = {
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    };
+
+    db.collection("comments")
+        .add(newComment)
+        .then(doc => {
+            res.json({ message: `document ${doc.id} created succesfully`});
+        })
+        .catch(err => {
+            res.status(500).json({ error: "something was wrong" });
+            console.error(err);
+        });
+});
