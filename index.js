4c10c8068322cd768ec34250374f61bbeefcbc56
Ariel Calisaya <calisayaariel62@gmail.com>
2020-01-28 11:40:30 -0300

    added express and deployed to firebase

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const serviceAccount = require("../key/service_account.json");
+const serviceAccount = require("./service_account.json");
+const express = require('express');
+
+const app = express();
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
-
-
 const db = admin.firestore();
 
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-});
-
-exports.getComments = functions.https.onRequest((req, res) => {
+app.get('/comments', (req, res) => {
     db.collection("comments")
         .get()
-        .then(data => {
+        .then((data) => {
             let comments = [];
-            data.forEach(doc => {
+            data.forEach((doc) => {
                 comments.push(doc.data());
             });
             return res.json(comments);
         })
-        .catch(err => console.error(err));
+        .catch((err) => console.error(err));
 });
 
 exports.createComment = functions.https.onRequest((req, res) => {
-    if(req.method !== 'POST'){
-        return res.status(400).json({error: 'Method not allowed'})
+    if (req.method !== "POST") {
+        return res.status(400).json({
+            error: "Method not allowed"
+        });
     }
 
     const newComment = {
@@ -41,11 +39,17 @@ exports.createComment = functions.https.onRequest((req, res) => {
 
     db.collection("comments")
         .add(newComment)
-        .then(doc => {
-            res.json({ message: `document ${doc.id} created succesfully`});
+        .then((doc) => {
+            res.json({
+                message: `document ${doc.id} created succesfully`
+            });
         })
-        .catch(err => {
-            res.status(500).json({ error: "something was wrong" });
+        .catch((err) => {
+            res.status(500).json({
+                error: "something was wrong"
+            });
             console.error(err);
         });
 });
+
+exports.api = functions.https.onRequest(app);
