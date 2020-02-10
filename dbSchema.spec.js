51e3602d2d20e0ce5d4d01f1ef51c92bef8c57e1
Ariel Calisaya <calisayaariel62@gmail.com>
2020-02-10 20:27:32 -0300

    spec of dbschema addded

---
+++ dbSchema.spec.js
@@ -0,0 +1,23 @@
+// example of a data base coments
+let user = {
+  userId: "4134t0ry20120021",
+  email: "uruser@gmail.com",
+  handle: "Ur_user",
+  createdAt: "2020-02-08T105:01:48.909Z",
+  imageUrl: "image/png",
+  bio: "here is my account",
+  website: "https://uruser.com",
+  localtion: "santiago, CL"
+}
+
+let db = {
+  comments: [
+    {
+      userHandle: "user",
+      body: "comment from db js",
+      createdAt: "2020-01-29T05:01:48.909Z",
+      liedCount: 5,
+      commentCount: 3
+    }
+  ]
+};
