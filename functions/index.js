49095735fcc972fcaafb776d9dcb5d0285fb71ac
Ariel Calisaya <calisayaariel62@gmail.com>
2020-02-19 05:41:32 -0300

    created function to notify the likes and comments

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const app = require('express')();
 const FBAuth = require('./utils/FirebaseAuth');
 const functions = require('firebase-functions');
+const { db } = require('./utils/admin');
 const {
     Signup, 
     Signin, 
@@ -36,7 +37,68 @@ app.post('/user/image', FBAuth, setImage);
 app.get('/user', FBAuth, GET_User);
 
 
-
-
-
 exports.api = functions.region('us-central1').https.onRequest(app);
+
+exports.notificationsOnLike = functions.region('us-central1').firestore.document('likes/{id}')
+    .onCreate((snapshot) => {
+        db.doc(`/comments/${snapshot.data().commentId}`).get()
+            .then(doc => {
+                if(doc.exists){
+                    return db.doc(`/notifications/${snapshot.id}`).set({
+                        createdAt: new Date().toISOString(),
+                        recipient: doc.data().userHandle,
+                        sender: snapshot.data().userHandle,
+                        type: 'like',
+                        read: false,
+                        commentId: doc.id
+                    }); 
+                }
+            })
+            .then(() => {
+                return;
+            })
+            .catch(err => {
+                console.error(err)
+                return;
+            })
+    });
+
+exports.notificationsOnComment = functions.region('us-central1')
+.firestore.document('userInComments/{id}')
+.onCreate((snapshot) => {
+    db.doc(`/comments/${snapshot.data().commentId}`)
+    .get()
+        .then(doc => {
+            if(doc.exists && doc.data().userHandle !== snapshot.data().userHandle){
+                return db.doc(`/notifications/${snapshot.id}`).set({
+                    createdAt: new Date().toISOString(),
+                    recipient: doc.data().userHandle,
+                    sender: snapshot.data().userHandle,
+                    type: 'comment',
+                    read: false,
+                    commentId: doc.id
+                }); 
+            }
+        })
+        .then(() => {
+            return;
+        })
+        .catch(err => {
+            console.error(err)
+            return;
+        })
+});
+
+exports.deleteNotificationsOnUnLike = functions.region('us-central1')
+.firestore.document('likes/{id}')
+.onDelete((snapshot) => {
+    db.doc(`notifications/${snapshot.id}`)
+        .delete()
+        .then(() => {
+            return;
+        })
+        .catch(err => {
+            console.error(err)
+            return;
+        })
+})
